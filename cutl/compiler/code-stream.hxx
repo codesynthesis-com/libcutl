d8fc8867ae9eb86e132a6ba676b6c5319346f7f0
Boris Kolpackov <boris@codesynthesis.com>
2011-01-04 16:33:32 +0200

    Copyright update

--- cutl/compiler/code-stream.hxx
+++ cutl/compiler/code-stream.hxx
@@ -1,6 +1,6 @@
 // file      : cutl/compiler/code-stream.hxx
 // author    : Boris Kolpackov <boris@codesynthesis.com>
-// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 // license   : MIT; see accompanying LICENSE file
 
 #ifndef CUTL_COMPILER_CODE_STREAM_HXX
--- cutl/container/graph.hxx
+++ cutl/container/graph.hxx
@@ -1,6 +1,6 @@
 // file      : cutl/container/graph.hxx
 // author    : Boris Kolpackov <boris@codesynthesis.com>
-// copyright : Copyright (c) 2009-2010 Code Synthesis Tools CC
+// copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
 // license   : MIT; see accompanying LICENSE file
 
 #ifndef CUTL_CONTAINER_GRAPH_HXX
