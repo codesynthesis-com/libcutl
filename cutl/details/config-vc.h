beb83e2fa2c2c3772bdfaf8c7e26af55b5c5cfd0
Boris Kolpackov <boris@codesynthesis.com>
2011-12-27 16:11:40 +0200

    Add details/config-vc.h file

---
+++ cutl/details/config-vc.h
@@ -0,0 +1,12 @@
+/* file      : cutl/details/config-vc.h
+ * author    : Boris Kolpackov <boris@codesynthesis.com>
+ * copyright : Copyright (c) 2009-2011 Code Synthesis Tools CC
+ * license   : MIT; see accompanying LICENSE file
+ */
+
+/* Configuration file for Windows/VC++. */
+
+#ifndef CUTL_DETAILS_CONFIG_VC_H
+#define CUTL_DETAILS_CONFIG_VC_H
+
+#endif /* CUTL_DETAILS_CONFIG_VC_H */
